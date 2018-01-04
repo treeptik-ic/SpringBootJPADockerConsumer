0a58517b7b7d3f67a5d6063a4cf35797f8b9ecad
c.lamaud <c.lamaud@treeptik.fr>
2018-01-04 15:41:24 +0100

    Mise en place du Post

--- src/main/java/fr/lamaud/controller/UserController.java
+++ src/main/java/fr/lamaud/controller/UserController.java
@@ -7,6 +7,9 @@ import javax.annotation.Resource;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.client.RestTemplate;
@@ -22,10 +25,19 @@ public class UserController {
 	private UserService userService;
 	
 	@RequestMapping(method = RequestMethod.GET)
-	public String welcome(Map<String, Object> model) throws Exception {
-		User user = this.userService.getUsers()[0];
-		model.put("user", user);
+	public String getUsers(Model model) throws Exception {
+		model.addAttribute("user", new User());
+		model.addAttribute("users", this.userService.getUsers());
 		return "welcome";
 	}
-
+	
+	@RequestMapping(method = RequestMethod.POST)
+	public String postUser(@ModelAttribute User user, Model model) throws Exception {
+		model.addAttribute("user", new User());
+		
+		this.userService.postUserToRest(user);
+		
+		model.addAttribute("users", this.userService.getUsers());
+		return "welcome";
+	}
 }
--- src/main/java/fr/lamaud/service/UserService.java
+++ src/main/java/fr/lamaud/service/UserService.java
@@ -2,6 +2,7 @@ package fr.lamaud.service;
 
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.client.ClientHttpRequestFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -10,14 +11,14 @@ import fr.lamaud.pojo.User;
 @Service(value="userService")
 public class UserService {
 
-	@Bean
-	public RestTemplate restTemplate(RestTemplateBuilder builder) {
-		return builder.build();
-	}
 
-	@Bean
 	public User[] getUsers() throws Exception {
 		RestTemplate restTemplate = new RestTemplate();
 		return restTemplate.getForObject("http://localhost:8090/user", User[].class);
+	}
+	
+	public void postUserToRest(User user) throws Exception {
+		RestTemplate restTemplate = new RestTemplate();
+		User result = restTemplate.postForObject("http://localhost:8090/user", user, User.class);
 	}
 }
